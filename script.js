9a6d6d0453bc48c77f94f2c32b7d4ec2fe05ef0e
Uddhav <uddhavsaikia@gmail.com>
2024-08-19 10:02:46 +0530

    commit images

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// When the user scrolls the page, execute myFunction
+// scroll progress bar
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -8,8 +8,7 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
-
-
+// for quiz
 const quizData = [
   {
       question: "Why do we love you?",
@@ -73,3 +72,32 @@ function showFinalScore() {
 }
 
 loadQuestion();
+
+//image slider part
+
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 3000; // duration(speed) of the slide
+
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) { // reset
+    imgs[i].style.opacity = 0;
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  currentImg = n;
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].className += ' active';
+}
+
+var timer = setInterval(changeSlide, interval);
+
+currentImg = (currentImg + 1) % imgs.length; // update the index number
+
+if (n != undefined) {
+    clearInterval(timer);
+    timer = setInterval(changeSlide, interval);
+    currentImg = n;
+}
