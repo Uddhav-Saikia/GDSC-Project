271d45269b773e77788db68619a7254c91133c78
Uddhav <uddhavsaikia@gmail.com>
2024-08-19 15:38:53 +0530

    debugging in proces

--- script.js
+++ script.js
@@ -78,12 +78,10 @@ loadQuestion();
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
